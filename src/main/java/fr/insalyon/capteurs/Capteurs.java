1c10ca358a93663b0dab58a8d9b4faaeab2f3504
Antoine Rochebois <Antoine Rochebois@10.43.6.88>
2019-05-24 17:17:14 +0200

    Premier commit

---
+++ src/main/java/fr/insalyon/capteurs/Capteurs.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insalyon.capteurs;
+
+public class Capteurs {
+    public static Fenetre F;
+    public static void main(String[] args) {
+         F = new Fenetre();
+    }
+    
+}

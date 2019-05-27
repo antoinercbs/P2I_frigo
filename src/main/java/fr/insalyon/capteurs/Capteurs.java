9f2bb76e41a42688afa7f05707df5882d8815316
Antoine Rochebois <Antoine Rochebois@10.43.6.88>
2019-05-27 16:51:59 +0200

    Ajout des lib et de la méthode pour get les json produits

--- src/main/java/fr/insalyon/capteurs/Capteurs.java
+++ src/main/java/fr/insalyon/capteurs/Capteurs.java
@@ -5,7 +5,17 @@
  */
 package fr.insalyon.capteurs;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import fr.insalyon.capteurs.view.Fenetre;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import org.json.JSONObject;
 
 public class Capteurs {
     
@@ -13,6 +23,38 @@ public class Capteurs {
     
     public static void main(String[] args) {
          F = new Fenetre();
+    }
+    
+    static void displayPopupFromGtin(String gtin) {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://fr.openfoodfacts.org/api/v0/produit/" + gtin + ".json")
+                .get()
+                .build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            String productDescription = response.body().string();
+            JSONObject product = new JSONObject(productDescription);
+
+            String name = product.getJSONObject("product").getString("product_name");
+            //String description = product.getJSONObject("product").getString("generic_name");
+
+            String url = product.getJSONObject("product").getString("image_front_url");
+            BufferedImage img = null;
+            img = ImageIO.read(new URL(url));
+            ImageIcon icon = new ImageIcon(img);
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Nom : " + name  /*"\nDescription : " + description*/,
+                    "PUTAIN LES GARS C'EST TROP BIEN JE VAIS CHIALLER !!!!!!", JOptionPane.INFORMATION_MESSAGE,
+                    icon);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
 }
